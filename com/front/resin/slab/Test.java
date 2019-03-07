efc8fe62a759c2ab1b2f00a5ae6fa8352af2cee9
dinghui115046 <dinghui12345>
2019-03-07 11:58:17 +0800

    提交代码

--- com/front/resin/slab/Test.java
+++ com/front/resin/slab/Test.java
@@ -4,29 +4,13 @@ package com.front.resin.slab;
  * Created by hinotohui on 18/7/17.
  */
 public class Test {
-    private int b=0;
-
-    public class A{
-        private int a;
-
-        public A(){
-            this.a=1;
-        }
-
-        public void a(){
-            b=2;
-        }
-    }
-
-    public  void test(){
-        System.out.println(b);
-    }
 
     public static void  main(String[]  args){
-        Test test=new Test();
-        Test.A a=test.new A();
-        a.a();
-        test.test();
+        Slab slab=new Slab();
 
+        for(int i=0;i<1000;i++){
+            SharedBuffer sharedBuffer=slab.allocate(125);
+            slab.free(sharedBuffer)
+        }
     }
 }
